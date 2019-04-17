af66072272a260232982fde994654aa787286e58
Ed Page <eopage@gmail.com>
2019-04-17 07:30:17 -0600

    feat(dict): Perform case-insensitive comparisons

--- build.rs
+++ build.rs
@@ -10,15 +10,21 @@ fn main() {
     let mut file = BufWriter::new(File::create(&path).unwrap());
 
     println!("rerun-if-changed=./assets/words.csv");
-    write!(&mut file, "static DICTIONARY: phf::Map<&'static str, &'static str> = ").unwrap();
+    write!(&mut file, "use unicase::UniCase;").unwrap();
+    write!(
+        &mut file,
+        "static DICTIONARY: phf::Map<unicase::UniCase<&'static str>, &'static str> = "
+    )
+    .unwrap();
     let mut builder = phf_codegen::Map::new();
-    let records: Vec<_> = csv::Reader::from_reader(CORPUS).records().map(|r| r.unwrap()).collect();
+    let records: Vec<_> = csv::Reader::from_reader(CORPUS)
+        .records()
+        .map(|r| r.unwrap())
+        .collect();
     for record in &records {
         let value = format!(r#""{}""#, &record[1]);
-        builder.entry(&record[0], &value);
+        builder.entry(unicase::UniCase(&record[0]), &value);
     }
-    builder
-        .build(&mut file)
-        .unwrap();
+    builder.build(&mut file).unwrap();
     write!(&mut file, ";\n").unwrap();
 }
--- src/dict.rs
+++ src/dict.rs
@@ -1,18 +1,34 @@
 include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
-pub struct Dictionary {
-}
+pub struct Dictionary {}
 
 impl Dictionary {
     pub fn new() -> Self {
-        Dictionary { }
+        Dictionary {}
     }
 
-    pub fn correct_str<'s>(&'s self, word: &str) -> Option<&'s str> {
-        DICTIONARY.get(word).map(|s| *s)
+    pub fn correct_str<'s, 'w>(&'s self, word: &'w str) -> Option<&'s str> {
+        map_lookup(&DICTIONARY, word)
     }
 
-    pub fn correct_bytes<'s>(&'s self, word: &[u8]) -> Option<&'s str> {
-        std::str::from_utf8(word).ok().and_then(|word| DICTIONARY.get(word)).map(|s| *s)
+    pub fn correct_bytes<'s, 'w>(&'s self, word: &'w [u8]) -> Option<&'s str> {
+        std::str::from_utf8(word)
+            .ok()
+            .and_then(|word| self.correct_str(word))
+    }
+}
+
+fn map_lookup(
+    map: &'static phf::Map<UniCase<&'static str>, &'static str>,
+    key: &str,
+) -> Option<&'static str> {
+    // This transmute should be safe as `get` will not store the reference with
+    // the expanded lifetime. This is due to `Borrow` being overly strict and
+    // can't have an impl for `&'static str` to `Borrow<&'a str>`.
+    //
+    // See https://github.com/rust-lang/rust/issues/28853#issuecomment-158735548
+    unsafe {
+        let key = ::std::mem::transmute::<_, &'static str>(key);
+        map.get(&UniCase(key)).map(|s| *s)
     }
 }
