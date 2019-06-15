63a84863be9a530a8e96201fe412421e051d0599
Ed Page <eopage@gmail.com>
2019-06-15 14:04:02 -0600

    refactor(bench): Improve bench names

--- benches/tokenize.rs
+++ benches/tokenize.rs
@@ -5,33 +5,33 @@ extern crate test;
 mod data;
 
 #[bench]
-fn tokenize_empty(b: &mut test::Bencher) {
+fn symbol_parse_empty(b: &mut test::Bencher) {
     b.iter(|| defenestrate::tokens::Symbol::parse(data::EMPTY.as_bytes()).collect::<Vec<_>>());
 }
 
 #[bench]
-fn tokenize_no_tokens(b: &mut test::Bencher) {
+fn symbol_parse_no_tokens(b: &mut test::Bencher) {
     b.iter(|| defenestrate::tokens::Symbol::parse(data::NO_TOKENS.as_bytes()).collect::<Vec<_>>());
 }
 
 #[bench]
-fn tokenize_single_token(b: &mut test::Bencher) {
+fn symbol_parse_single_token(b: &mut test::Bencher) {
     b.iter(|| {
         defenestrate::tokens::Symbol::parse(data::SINGLE_TOKEN.as_bytes()).collect::<Vec<_>>()
     });
 }
 
 #[bench]
-fn tokenize_sherlock(b: &mut test::Bencher) {
+fn symbol_parse_sherlock(b: &mut test::Bencher) {
     b.iter(|| defenestrate::tokens::Symbol::parse(data::SHERLOCK.as_bytes()).collect::<Vec<_>>());
 }
 
 #[bench]
-fn tokenize_code(b: &mut test::Bencher) {
+fn symbol_parse_code(b: &mut test::Bencher) {
     b.iter(|| defenestrate::tokens::Symbol::parse(data::CODE.as_bytes()).collect::<Vec<_>>());
 }
 
 #[bench]
-fn tokenize_corpus(b: &mut test::Bencher) {
+fn symbol_parse_corpus(b: &mut test::Bencher) {
     b.iter(|| defenestrate::tokens::Symbol::parse(data::CORPUS.as_bytes()).collect::<Vec<_>>());
 }
