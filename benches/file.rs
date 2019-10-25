a60ab52c5684f4ef5738e0da44c4dfc9efb589e1
Ed Page <epage@duosecurity.com>
2019-10-25 15:48:39 -0600

    test: Add benchmarks for real-word processing

--- benches/file.rs
+++ benches/file.rs
@@ -4,7 +4,8 @@ extern crate test;
 
 mod data;
 
-pub use assert_fs::prelude::*;
+use assert_fs::prelude::*;
+use bstr::ByteSlice;
 
 #[bench]
 fn check_file_empty(b: &mut test::Bencher) {
@@ -88,4 +89,292 @@ fn check_file_corpus(b: &mut test::Bencher) {
     b.iter(|| checks.check_file(sample_path.path(), true, typos::report::print_silent));
 
     temp.close().unwrap();
+}
+
+#[bench]
+fn read_empty(b: &mut test::Bencher) {
+    let temp = assert_fs::TempDir::new().unwrap();
+    let sample_path = temp.child("sample");
+    sample_path.write_str(data::EMPTY).unwrap();
+
+    b.iter(|| std::fs::read(sample_path.path()));
+
+    temp.close().unwrap();
+}
+
+#[bench]
+fn read_no_tokens(b: &mut test::Bencher) {
+    let temp = assert_fs::TempDir::new().unwrap();
+    let sample_path = temp.child("sample");
+    sample_path.write_str(data::NO_TOKENS).unwrap();
+
+    b.iter(|| std::fs::read(sample_path.path()));
+
+    temp.close().unwrap();
+}
+
+#[bench]
+fn read_single_token(b: &mut test::Bencher) {
+    let temp = assert_fs::TempDir::new().unwrap();
+    let sample_path = temp.child("sample");
+    sample_path.write_str(data::SINGLE_TOKEN).unwrap();
+
+    b.iter(|| std::fs::read(sample_path.path()));
+
+    temp.close().unwrap();
+}
+
+#[bench]
+fn read_sherlock(b: &mut test::Bencher) {
+    let temp = assert_fs::TempDir::new().unwrap();
+    let sample_path = temp.child("sample");
+    sample_path.write_str(data::SHERLOCK).unwrap();
+
+    b.iter(|| std::fs::read(sample_path.path()));
+
+    temp.close().unwrap();
+}
+
+#[bench]
+fn read_code(b: &mut test::Bencher) {
+    let temp = assert_fs::TempDir::new().unwrap();
+    let sample_path = temp.child("sample");
+    sample_path.write_str(data::CODE).unwrap();
+
+    b.iter(|| std::fs::read(sample_path.path()));
+
+    temp.close().unwrap();
+}
+
+#[bench]
+fn read_corpus(b: &mut test::Bencher) {
+    let temp = assert_fs::TempDir::new().unwrap();
+    let sample_path = temp.child("sample");
+    sample_path.write_str(data::CORPUS).unwrap();
+
+    b.iter(|| std::fs::read(sample_path.path()));
+
+    temp.close().unwrap();
+}
+
+#[bench]
+fn split_lines_empty(b: &mut test::Bencher) {
+    b.iter(|| data::EMPTY.as_bytes().lines().enumerate().last());
+}
+
+#[bench]
+fn split_lines_no_tokens(b: &mut test::Bencher) {
+    b.iter(|| data::NO_TOKENS.as_bytes().lines().enumerate().last());
+}
+
+#[bench]
+fn split_lines_single_token(b: &mut test::Bencher) {
+    b.iter(|| data::SINGLE_TOKEN.as_bytes().lines().enumerate().last());
+}
+
+#[bench]
+fn split_lines_sherlock(b: &mut test::Bencher) {
+    b.iter(|| data::SHERLOCK.as_bytes().lines().enumerate().last());
+}
+
+#[bench]
+fn split_lines_code(b: &mut test::Bencher) {
+    b.iter(|| data::CODE.as_bytes().lines().enumerate().last());
+}
+
+#[bench]
+fn split_lines_corpus(b: &mut test::Bencher) {
+    b.iter(|| data::CORPUS.as_bytes().lines().enumerate().last());
+}
+
+#[bench]
+fn parse_empty(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::EMPTY
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).last();
+                ()
+            })
+    });
+}
+
+#[bench]
+fn parse_no_tokens(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::NO_TOKENS
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).last();
+                ()
+            })
+    });
+}
+
+#[bench]
+fn parse_single_token(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::SINGLE_TOKEN
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).last();
+                ()
+            })
+    });
+}
+
+#[bench]
+fn parse_sherlock(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::SHERLOCK
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).last();
+                ()
+            })
+    });
+}
+
+#[bench]
+fn parse_code(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::CODE
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).last();
+                ()
+            })
+    });
+}
+
+#[bench]
+fn parse_corpus(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::CORPUS
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).last();
+                ()
+            })
+    });
+}
+
+#[bench]
+fn split_empty(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::EMPTY
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).for_each(|l| {
+                    l.split().last();
+                    ()
+                })
+            })
+    });
+}
+
+#[bench]
+fn split_no_tokens(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::NO_TOKENS
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).for_each(|l| {
+                    l.split().last();
+                    ()
+                })
+            })
+    });
+}
+
+#[bench]
+fn split_single_token(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::SINGLE_TOKEN
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).for_each(|l| {
+                    l.split().last();
+                    ()
+                })
+            })
+    });
+}
+
+#[bench]
+fn split_sherlock(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::SHERLOCK
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).for_each(|l| {
+                    l.split().last();
+                    ()
+                })
+            })
+    });
+}
+
+#[bench]
+fn split_code(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::CODE
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).for_each(|l| {
+                    l.split().last();
+                    ()
+                })
+            })
+    });
+}
+
+#[bench]
+fn split_corpus(b: &mut test::Bencher) {
+    let parser = typos::tokens::Parser::new();
+    b.iter(|| {
+        data::CORPUS
+            .as_bytes()
+            .lines()
+            .enumerate()
+            .for_each(|(_idx, l)| {
+                parser.parse_bytes(l).for_each(|l| {
+                    l.split().last();
+                    ()
+                })
+            })
+    });
 }
