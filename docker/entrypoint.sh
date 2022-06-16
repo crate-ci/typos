fc7f51746699c0dacd6a3c2a8f594a18c1726336
Ed Page <eopage@gmail.com>
2022-06-16 10:09:50 -0500

    perf(action): Limit checked files to those changed

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,6 +8,15 @@ log() {
 
 CMD_NAME="typos"
 TARGET=${INPUT_FILES:-"."}
+if [[ -n "${GITHUB_BASE_REF:-}" ]]; then
+    git config --global --add safe.directory /github/workspace
+    if git rev-parse --verify master 2>/dev/null ; then
+        log "Limiting checks to ${GITHUB_BASE_REF}...HEAD"
+        TARGET=$(git diff ${GITHUB_BASE_REF}...HEAD --name-only --diff-filter=AM -- ${TARGET})
+    else
+        log "WARN: Not limiting checks to ${GITHUB_BASE_REF}...HEAD, ${GITHUB_BASE_REF} is not available"
+    fi
+fi
 
 if [[ -z $(ls ${TARGET} 2>/dev/null) ]]; then
     log "ERROR: Input files (${TARGET}) not found"
