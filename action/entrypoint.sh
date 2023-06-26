31584bbba585559fa926d64c6e7460d5fa22795c
Michael Voříšek <mvorisek@mvorisek.cz>
2023-06-27 01:11:45 +0200

    Fix wget usage for Alpine

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -36,7 +36,12 @@ fi
 if [[ ! -x ${COMMAND} ]]; then
     VERSION=1.15.7
     log "Downloading 'typos' v${VERSION}"
-    wget --progress=dot:mega "https://github.com/crate-ci/typos/releases/download/v${VERSION}/typos-v${VERSION}-x86_64-unknown-linux-musl.tar.gz"
+    if [ -f /etc/alpine-release ]; then
+        wget "https://github.com/crate-ci/typos/releases/download/v${VERSION}/typos-v${VERSION}-x86_64-unknown-linux-musl.tar.gz"
+    else
+        wget --progress=dot:mega "https://github.com/crate-ci/typos/releases/download/v${VERSION}/typos-v${VERSION}-x86_64-unknown-linux-musl.tar.gz"
+    fi
+
     mkdir -p ${_INSTALL_DIR}
     tar -xzvf typos-v${VERSION}-x86_64-unknown-linux-musl.tar.gz -C ${_INSTALL_DIR} ./${CMD_NAME}
     rm typos-v${VERSION}-x86_64-unknown-linux-musl.tar.gz
