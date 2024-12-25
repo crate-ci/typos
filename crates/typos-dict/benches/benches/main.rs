661825438ce74fc616ff3bddd0d8d0747b87e150
Ed Page <eopage@gmail.com>
2024-12-24 21:21:54 -0600

    perf(dict): Add basic hit/miss benches

--- crates/typos-dict/benches/benches/main.rs
+++ crates/typos-dict/benches/benches/main.rs
@@ -1,3 +1,22 @@
+#![allow(clippy::wildcard_imports)]
+
+const MISS: &str = "finalizes";
+const HIT: &str = "finallizes";
+
+mod trie {
+    use super::*;
+
+    #[divan::bench(args = [unicase::UniCase::new(MISS)])]
+    fn miss(word: unicase::UniCase<&str>) -> Option<&'static &[&str]> {
+        typos_dict::WORD_TRIE.find(&word)
+    }
+
+    #[divan::bench(args = [unicase::UniCase::new(HIT)])]
+    fn hit(word: unicase::UniCase<&str>) -> Option<&'static &[&str]> {
+        typos_dict::WORD_TRIE.find(&word)
+    }
+}
+
 fn main() {
     divan::main();
 }
