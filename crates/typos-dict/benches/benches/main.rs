4330c4b8d591fc9f6e72ddf8eb3a0c80420d742a
Ed Page <eopage@gmail.com>
2025-02-13 10:46:15 -0600

    bench: Remove cased_map

--- crates/typos-dict/benches/benches/main.rs
+++ crates/typos-dict/benches/benches/main.rs
@@ -2,7 +2,6 @@
 #![allow(dead_code)]
 
 mod aho_corasick_codegen;
-mod cased_map_codegen;
 mod map_codegen;
 
 static AHO_CORASICK: std::sync::LazyLock<aho_corasick_codegen::Word> =
@@ -22,11 +21,6 @@ mod miss {
 
     const MISS: &str = "finalizes";
 
-    #[divan::bench(args = [MISS])]
-    fn cased_map(word: &str) -> Option<&'static &[&str]> {
-        cased_map_codegen::WORD_ASCII_LOWER.find(&word)
-    }
-
     #[divan::bench(args = [unicase::UniCase::new(MISS)])]
     fn map(word: unicase::UniCase<&str>) -> Option<&'static &[&str]> {
         map_codegen::WORD.find(&word)
@@ -42,11 +36,6 @@ mod hit {
     use super::*;
 
     const HIT: &str = "finallizes";
-
-    #[divan::bench(args = [HIT])]
-    fn cased_map(word: &str) -> Option<&'static &[&str]> {
-        cased_map_codegen::WORD_ASCII_LOWER.find(&word)
-    }
 
     #[divan::bench(args = [unicase::UniCase::new(HIT)])]
     fn map(word: unicase::UniCase<&str>) -> Option<&'static &[&str]> {
