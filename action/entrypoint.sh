718c4ff697435edabd4f1c52c3775521adbb33a3
Ed Page <eopage@gmail.com>
2025-03-28 12:20:39 -0500

    chore: Release

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -20,7 +20,7 @@ if [[ -z $(ls ${TARGET} 2>/dev/null) ]]; then
 fi
 
 if [[ ! -x ${COMMAND} ]]; then
-    VERSION=1.30.3
+    VERSION=1.31.0
     if [[ "$(uname -m)" == "arm64" || "$(uname -m)" == "aarch64" ]]; then
         ARCH="aarch64"
     else
--- docs/pre-commit.md
+++ docs/pre-commit.md
@@ -6,7 +6,7 @@ config at this repository:
 ```yaml
 repos:
   - repo: https://github.com/crate-ci/typos
-    rev: v1.30.3
+    rev: v1.31.0
     hooks:
       - id: typos
 ```
