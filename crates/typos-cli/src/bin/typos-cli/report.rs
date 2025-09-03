7f5f1ecb364815c9cd56c3b82076c7418fde55be
Ed Page <eopage@gmail.com>
2025-09-03 14:23:45 -0500

    fix(cli): Replace error renderer with annotate-snippets
    This is rustc/cargo's renderer and has all of the lessons of those,
    including
    - Unicode support
    - Line wrapping / eliding of text to fit on screen
    
    As part of this, we are now rendering file name typos like file
    content typos.

--- crates/typos-cli/src/bin/typos-cli/report.rs
+++ crates/typos-cli/src/bin/typos-cli/report.rs
@@ -1,17 +1,21 @@
 #![allow(clippy::needless_update)]
 
+use std::borrow::Cow;
 use std::io::Write as _;
 use std::sync::{atomic, Mutex};
 
+use annotate_snippets::AnnotationKind;
+use annotate_snippets::Group;
+use annotate_snippets::Level;
+use annotate_snippets::Origin;
+use annotate_snippets::Snippet;
+use anstream::stderr;
 use anstream::stdout;
 use serde_sarif::sarif;
 use serde_sarif::sarif::{ArtifactChange, ArtifactContent, Fix, Replacement};
-use typos_cli::report::{Context, Message, Report, Typo};
-use unicode_width::UnicodeWidthStr;
+use typos_cli::report::{Context, Error, Message, Report, Typo};
 
-const ERROR: anstyle::Style = anstyle::AnsiColor::BrightRed.on_default();
 const INFO: anstyle::Style = anstyle::AnsiColor::BrightBlue.on_default();
-const GOOD: anstyle::Style = anstyle::AnsiColor::BrightGreen.on_default();
 
 pub(crate) struct MessageStatus<'r> {
     typos_found: atomic::AtomicBool,
@@ -66,11 +70,15 @@ pub(crate) struct PrintBrief;
 
 impl Report for PrintBrief {
     fn report(&self, msg: Message<'_>) -> Result<(), std::io::Error> {
+        let renderer = RENDERER.clone().short_message(true);
         match &msg {
             Message::BinaryFile(msg) => {
                 log::info!("{msg}");
             }
-            Message::Typo(msg) => print_brief_correction(msg)?,
+            Message::Typo(msg) => {
+                let report = &[typo_to_group(msg)];
+                writeln!(stdout(), "{}", renderer.render(report))?;
+            }
             Message::FileType(msg) => {
                 let info = INFO.render();
                 let reset = anstyle::Reset.render();
@@ -88,7 +96,8 @@ impl Report for PrintBrief {
                 writeln!(stdout().lock(), "{}", msg.data)?;
             }
             Message::Error(msg) => {
-                log::error!("{}: {}", context_display(&msg.context), msg.msg);
+                let report = &[error_to_group(msg)];
+                writeln!(stderr(), "{}", renderer.render(report))?;
             }
             _ => unimplemented!("New message {:?}", msg),
         }
@@ -104,7 +113,10 @@ impl Report for PrintLong {
             Message::BinaryFile(msg) => {
                 log::info!("{msg}");
             }
-            Message::Typo(msg) => print_long_correction(msg)?,
+            Message::Typo(msg) => {
+                let report = &[typo_to_group(msg)];
+                writeln!(stdout(), "{}", RENDERER.render(report))?;
+            }
             Message::FileType(msg) => {
                 let info = INFO.render();
                 let reset = anstyle::Reset.render();
@@ -122,7 +134,8 @@ impl Report for PrintLong {
                 writeln!(stdout().lock(), "{}", msg.data)?;
             }
             Message::Error(msg) => {
-                log::error!("{}: {}", context_display(&msg.context), msg.msg);
+                let report = &[error_to_group(msg)];
+                writeln!(stderr(), "{}", RENDERER.render(report))?;
             }
             _ => unimplemented!("New message {:?}", msg),
         }
@@ -130,150 +143,84 @@ impl Report for PrintLong {
     }
 }
 
-fn print_brief_correction(msg: &Typo<'_>) -> Result<(), std::io::Error> {
-    let error = ERROR.render();
-    let good = GOOD.render();
-    let info = INFO.render();
-    let reset = anstyle::Reset.render();
-
-    let start = String::from_utf8_lossy(&msg.buffer[0..msg.byte_offset]);
-    let column_number =
-        unicode_segmentation::UnicodeSegmentation::graphemes(start.as_ref(), true).count() + 1;
-    match &msg.corrections {
-        typos::Status::Valid => {}
+fn typo_to_group<'t>(msg: &'t Typo<'t>) -> Group<'t> {
+    let title = match &msg.corrections {
+        typos::Status::Valid => unimplemented!("never valid words to report"),
         typos::Status::Invalid => {
-            let divider = ":";
-            writeln!(
-                stdout().lock(),
-                "{info}{}{divider}{column_number}{reset}: `{error}{}{reset}` is disallowed",
-                context_display(&msg.context),
-                msg.typo,
-            )?;
+            format!("`{}` is disallowed", msg.typo,)
         }
         typos::Status::Corrections(corrections) => {
-            let divider = ":";
-            writeln!(
-                stdout().lock(),
-                "{info}{}{divider}{column_number}{reset}: `{error}{}{reset}` -> {}",
-                context_display(&msg.context),
+            format!(
+                "`{}` should be {}",
                 msg.typo,
-                itertools::join(
-                    corrections.iter().map(|s| format!("`{good}{s}{reset}`")),
-                    ", "
-                )
-            )?;
+                itertools::join(corrections.iter().map(|s| format!("`{s}`")), ", ")
+            )
         }
-    }
-
-    Ok(())
+    };
+    let group = Group::with_title(Level::ERROR.primary_title(Cow::Owned(title)));
+    let span_start = msg.byte_offset;
+    let span_end = span_start + msg.typo.len();
+    let span = span_start..span_end;
+    let group = match &msg.context {
+        Some(Context::File(context)) => {
+            let path = context.path.as_os_str().to_string_lossy();
+            let line = String::from_utf8_lossy(msg.buffer.as_ref());
+            group.element(
+                Snippet::source(line)
+                    .path(path)
+                    .line_start(context.line_num)
+                    .annotation(AnnotationKind::Primary.span(span)),
+            )
+        }
+        Some(Context::Path(context)) => {
+            let path = context
+                .path
+                .parent()
+                .unwrap_or(std::path::Path::new("."))
+                .join("");
+            let path = match path.into_os_string().into_string() {
+                Ok(path) => path,
+                Err(path) => path.to_string_lossy().into_owned(),
+            };
+            let line = context.path.file_name().unwrap().to_string_lossy();
+            group.element(
+                Snippet::source(line)
+                    .path(Cow::Owned(path))
+                    .annotation(AnnotationKind::Primary.span(span)),
+            )
+        }
+        Some(_) | None => group,
+    };
+    group
 }
 
-fn print_long_correction(msg: &Typo<'_>) -> Result<(), std::io::Error> {
-    let error = ERROR.render();
-    let good = GOOD.render();
-    let info = INFO.render();
-    let reset = anstyle::Reset.render();
-
-    let stdout = stdout();
-    let mut handle = stdout.lock();
-
-    let line = String::from_utf8_lossy(msg.buffer.as_ref());
-    let line = line.replace('\t', " ");
-    let start = String::from_utf8_lossy(&msg.buffer[0..msg.byte_offset]);
-    let column_number =
-        unicode_segmentation::UnicodeSegmentation::graphemes(start.as_ref(), true).count() + 1;
-    match &msg.corrections {
-        typos::Status::Valid => {}
-        typos::Status::Invalid => {
-            writeln!(
-                handle,
-                "{error}error{reset}: `{error}{}{reset}` is disallowed",
-                msg.typo,
-            )?;
+fn error_to_group<'e>(error: &'e Error<'e>) -> Group<'e> {
+    let group = Group::with_title(Level::ERROR.primary_title(&error.msg));
+    match &error.context {
+        Some(Context::File(context)) => group.element(
+            Origin::path(context.path.as_os_str().to_string_lossy()).line(context.line_num),
+        ),
+        Some(Context::Path(context)) => {
+            group.element(Origin::path(context.path.as_os_str().to_string_lossy()))
         }
-        typos::Status::Corrections(corrections) => {
-            writeln!(
-                handle,
-                "{error}error{reset}: `{error}{}{reset}` should be {}",
-                msg.typo,
-                itertools::join(
-                    corrections.iter().map(|s| format!("`{good}{s}{reset}`")),
-                    ", "
-                )
-            )?;
-        }
+        Some(_) | None => group,
     }
-    let divider = ":";
-    writeln!(
-        handle,
-        "{info}  --> {reset}{}{divider}{column_number}",
-        context_display(&msg.context),
-    )?;
-
-    if let Some(Context::File(context)) = &msg.context {
-        let line_num = context.line_num.to_string();
-        let line_indent: String = itertools::repeat_n(" ", line_num.len()).collect();
-        let line = line.trim_end();
-
-        let visible_column = calculate_visible_column_width(start.as_ref());
-        let visible_len = calculate_visible_column_width(msg.typo);
-
-        let hl_indent: String = itertools::repeat_n(" ", visible_column).collect();
-        let hl: String = itertools::repeat_n("^", visible_len).collect();
-
-        writeln!(handle, "{info}{line_indent} |{reset}")?;
-        writeln!(handle, "{info}{line_num} |{reset} {line}")?;
-        writeln!(
-            handle,
-            "{info}{line_indent} |{reset} {hl_indent}{error}{hl}{reset}",
-        )?;
-        writeln!(handle, "{info}{line_indent} |{reset}")?;
-    }
-
-    Ok(())
 }
 
-fn calculate_visible_column_width(str: &str) -> usize {
-    let mut result = 0;
-    let graphemes = unicode_segmentation::UnicodeSegmentation::graphemes(str, true);
-    for grapheme in graphemes {
-        result += if grapheme == "\t" {
-            // TODO: config tab width
-            1
-        } else if is_emoji(grapheme) {
-            // UnicodeWidthStr::width doesn't cover for emoji according to their README.
-            // See: https://github.com/unicode-rs/unicode-width#unicode-width
-            // Also, the actual rendered column width may differ from calculation, especially for emojis.
-            // In here, we expect emoji renderers should render this emoji properly.
-            2
+static RENDERER: std::sync::LazyLock<annotate_snippets::Renderer> =
+    std::sync::LazyLock::new(|| {
+        let width = terminal_size::terminal_size()
+            .map(|(w, _)| w.0 as usize)
+            .unwrap_or(annotate_snippets::renderer::DEFAULT_TERM_WIDTH);
+        let decor_style = if supports_unicode::supports_unicode() {
+            annotate_snippets::renderer::DecorStyle::Unicode
         } else {
-            UnicodeWidthStr::width(grapheme)
-        }
-    }
-
-    result
-}
-
-fn is_emoji(grapheme: &str) -> bool {
-    if grapheme.is_ascii() {
-        return false;
-    }
-
-    for ch in grapheme.chars() {
-        if unic_emoji_char::is_emoji(ch) {
-            return true;
-        }
-    }
-
-    false
-}
-
-fn context_display<'c>(context: &'c Option<Context<'c>>) -> &'c dyn std::fmt::Display {
-    context
-        .as_ref()
-        .map(|c| c as &dyn std::fmt::Display)
-        .unwrap_or(&"")
-}
+            annotate_snippets::renderer::DecorStyle::Ascii
+        };
+        annotate_snippets::Renderer::styled()
+            .term_width(width)
+            .decor_style(decor_style)
+    });
 
 #[derive(Copy, Clone, Debug)]
 pub(crate) struct PrintJson;
@@ -498,82 +445,5 @@ fn typo_to_sarif_location(msg: &Typo<'_>) -> Result<sarif::Location, Box<dyn std
             .physical_location(physical.build())
             .build();
         Ok(location)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_calculate_visible_column_width_visible_ascii() {
-        for c in '!'..'~' {
-            assert_eq!(1, calculate_visible_column_width(&c.to_string()));
-        }
-    }
-
-    #[test]
-    fn test_calculate_visible_column_width_horizontal_tab() {
-        assert_eq!(1, calculate_visible_column_width("\t"));
-    }
-
-    #[test]
-    fn test_calculate_visible_column_width_latin_cyrillic() {
-        let latin_cyrillic_chars = [
-            "À", /* U+00C0; Latin Capital Letter A with Grave */
-            "À", /* U+0041 U+0300; Latin Capital Letter A, Combining Grave Accent */
-            "А", /* U+0410 Cyrillic Capital Letter A */
-        ];
-        for (i, ch) in latin_cyrillic_chars.iter().enumerate() {
-            let width = calculate_visible_column_width(ch);
-            assert_eq!(1, width, "latin_cyrillic[{i}]: {ch}",);
-        }
-    }
-
-    #[test]
-    fn test_calculate_visible_column_width_cjk() {
-        let cjk_chars = [
-            "中", /* U+4E2D */
-            "あ", /* U+3042 */
-            "한", /* U+1F635 U+200D U+1F4AB, NFC Korean */
-            "한", /* U+1F441 U+FE0F U+200D U+1F5E8 U+FE0F, NFD Korean */
-        ];
-        for (i, ch) in cjk_chars.iter().enumerate() {
-            let width = calculate_visible_column_width(ch);
-            assert_eq!(2, width, "cjk[{i}]: {ch}");
-        }
-    }
-
-    #[test]
-    fn test_calculate_visible_column_width_simple_emojis() {
-        // First non-component emojis of each groups in "Full Emoji List, v14.0"
-        // https://unicode.org/Public/emoji/14.0/emoji-test.txt
-        let simple_emojis = [
-            "😀", /* U+1F600 */
-            "👋", /* U+1F44B */
-            "🐵", /* U+1F435 */
-            "🍇", /* U+1F347 */
-            "🌍", /* U+1F30D */
-            "🎃", /* U+1F383 */
-            "👓", /* U+1F453 */
-            "🏧", /* U+1F3E7 */
-            "🏁", /* U+1F3C1 */
-        ];
-        for (i, ch) in simple_emojis.iter().enumerate() {
-            let width = calculate_visible_column_width(ch);
-            assert_eq!(2, width, "emoji[{i}]: {ch}");
-        }
-    }
-
-    #[test]
-    fn test_calculate_visible_column_width_zwj_sequences() {
-        let zwj_sequences = [
-            "😵‍💫", /* U+1F635 U+200D U+1F4AB */
-            "👁️‍🗨️", /* U+1F441 U+FE0F U+200D U+1F5E8 U+FE0F */
-        ];
-        for (i, ch) in zwj_sequences.iter().enumerate() {
-            let width = calculate_visible_column_width(ch);
-            assert_eq!(2, width, "zwj[{i}]: {ch}");
-        }
     }
 }
