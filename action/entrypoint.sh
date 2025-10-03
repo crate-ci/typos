7436548694def3314aacd93ed06c721b1f91ea04
Ed Page <eopage@gmail.com>
2025-10-03 14:03:58 -0500

    chore: Release

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -20,7 +20,7 @@ if [[ -z $(ls ${TARGET} 2>/dev/null) ]]; then
 fi
 
 if [[ ! -x ${COMMAND} ]]; then
-    VERSION=1.37.1
+    VERSION=1.37.2
     if [[ "$(uname -m)" == "arm64" || "$(uname -m)" == "aarch64" ]]; then
         ARCH="aarch64"
     else
