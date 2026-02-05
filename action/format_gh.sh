67e0ce729401c40040c633ef6a710329d9657a12
Andreas Dangel <andreas.dangel@adangel.org>
2026-02-05 12:10:26 +0100

    fix(gh): Fix col offset (1-based)

--- action/format_gh.sh
+++ action/format_gh.sh
@@ -6,5 +6,5 @@ grep '"type":"typo"' | while IFS= read -r typo; do
   original_path="$(echo "$typo" | jq --raw-output '.path')"
   relative_path="$(realpath --relative-to="$GITHUB_WORKSPACE" "$original_path")"
   echo "$typo" | jq --arg relative_path "$relative_path" --raw-output \
-    '"::warning file=\($relative_path),line=\(.line_num),col=\(.byte_offset)::\"\(.typo)\" should be \"" + (.corrections // [] | join("\" or \"") + "\".")'
+    '"::warning file=\($relative_path),line=\(.line_num),col=\(.byte_offset + 1)::\"\(.typo)\" should be \"" + (.corrections // [] | join("\" or \"") + "\".")'
 done
